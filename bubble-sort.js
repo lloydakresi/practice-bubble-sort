933f3b03d2652d59fb8a744604ddad9f6612caef
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-03 22:12:29 +0000

    complete

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,25 @@
 
 function bubbleSort(arr) {
+    let sorted;
 
     // Iterate through the array
-
+    while(!sorted){
+      sorted = true
+      for (let i = 0; i < arr.length; i++){
       // If the current value is greater than its neighbor to the right
         // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
+        if (arr[i] > arr[i + 1]){
+          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+          sorted = false;
+          console.log(arr.join(","));
+        }
+      }
+    // Do not move this console.log
     // If you get to the end of the array and no swaps have occurred, return
-
     // Otherwise, repeat from the beginning
+    }
 
+  return arr;
 }
 
 module.exports = bubbleSort;
